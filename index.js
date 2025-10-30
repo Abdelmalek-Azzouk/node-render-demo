bb3cbecdc96b93ff399185e72650f4542cde10b7
Malek <azzoukabdelmalek@gmail.com>
2025-10-30 20:35:35 +0100

    IDK

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
-res.send("\n NIGGER !");
+res.send("\n Bonjour depuis Render !");
 });
 app.listen(PORT, () => {
 console.log(`Serveur lancé sur le port ${PORT}`);
